fb75be6a58dce87c4b753eb3ccd4420261e3905c
Bhuvanesh <bhuvaneshcse03@gmail.com>
2025-01-28 22:15:51 +0530

    Pages creation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import './app.scss';
+import {createBrowserRouter , RouterProvider , Outlet} from 'react-router-dom';
+import { Navbar } from './home/Navbar';
+import { Login } from './auth/Login';
+import { Register } from './auth/Register';
+import { Home } from './home/Home';
 
 function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar/>
+        <Outlet/>
+      </div>
+    );
+  }
+
+  const router = createBrowserRouter([{
+    path:'/',
+    element: <Layout/>,
+    children: [
+      {
+        path:'/login',
+        element: <Login/>
+      },
+      {
+        path:'/register',
+        element: <Register/>
+      },
+      {
+        path:'/',
+        element: <Home/>
+      }
+    ]
+  }])
   return (
     <div className="App">
-      hello moto
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
